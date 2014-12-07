7bb7e2c342efaa29fc4772abde5db4abaff2ff1f
Erik Rossi <erikdenfredlige@gmail.com>
2014-12-07 21:04:16 +0100

    UI developed. Functionality to be implemented.

--- ProfileBuddy/GenerateRoute.Designer.cs
+++ ProfileBuddy/GenerateRoute.Designer.cs
@@ -33,14 +33,23 @@
             this.minimumLevelLabel = new System.Windows.Forms.Label();
             this.maximumLevelLabel = new System.Windows.Forms.Label();
             this.mailListBox = new System.Windows.Forms.CheckedListBox();
-            this.label1 = new System.Windows.Forms.Label();
-            this.label2 = new System.Windows.Forms.Label();
+            this.mailWhichItemsLabel = new System.Windows.Forms.Label();
+            this.sellWhichItemsLabel = new System.Windows.Forms.Label();
             this.sellListBox = new System.Windows.Forms.CheckedListBox();
             this.nameOfRouteTB = new System.Windows.Forms.TextBox();
             this.textBox2 = new System.Windows.Forms.TextBox();
             this.textBox3 = new System.Windows.Forms.TextBox();
             this.generateButton = new System.Windows.Forms.Button();
             this.backButton = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.minimumDurabilityLabel = new System.Windows.Forms.Label();
+            this.minimumBagSlotsLabel = new System.Windows.Forms.Label();
+            this.numberOfHotspotsLabel = new System.Windows.Forms.Label();
+            this.numberOfHotspots = new System.Windows.Forms.Label();
+            this.addHotspotButton = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox5 = new System.Windows.Forms.TextBox();
             this.SuspendLayout();
             // 
             // nameLabel
@@ -86,25 +95,25 @@
             this.mailListBox.Size = new System.Drawing.Size(120, 94);
             this.mailListBox.TabIndex = 4;
             // 
-            // label1
+            // mailWhichItemsLabel
             // 
-            this.label1.AutoSize = true;
-            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(201, 226);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(149, 20);
-            this.label1.TabIndex = 5;
-            this.label1.Text = "Mail which items?";
+            this.mailWhichItemsLabel.AutoSize = true;
+            this.mailWhichItemsLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.mailWhichItemsLabel.Location = new System.Drawing.Point(201, 226);
+            this.mailWhichItemsLabel.Name = "mailWhichItemsLabel";
+            this.mailWhichItemsLabel.Size = new System.Drawing.Size(149, 20);
+            this.mailWhichItemsLabel.TabIndex = 5;
+            this.mailWhichItemsLabel.Text = "Mail which items?";
             // 
-            // label2
+            // sellWhichItemsLabel
             // 
-            this.label2.AutoSize = true;
-            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label2.Location = new System.Drawing.Point(8, 226);
-            this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(147, 20);
-            this.label2.TabIndex = 6;
-            this.label2.Text = "Sell which items?";
+            this.sellWhichItemsLabel.AutoSize = true;
+            this.sellWhichItemsLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.sellWhichItemsLabel.Location = new System.Drawing.Point(8, 226);
+            this.sellWhichItemsLabel.Name = "sellWhichItemsLabel";
+            this.sellWhichItemsLabel.Size = new System.Drawing.Size(147, 20);
+            this.sellWhichItemsLabel.TabIndex = 6;
+            this.sellWhichItemsLabel.Text = "Sell which items?";
             // 
             // sellListBox
             // 
@@ -130,25 +139,25 @@
             // 
             this.textBox2.Location = new System.Drawing.Point(92, 35);
             this.textBox2.Name = "textBox2";
-            this.textBox2.Size = new System.Drawing.Size(255, 20);
+            this.textBox2.Size = new System.Drawing.Size(50, 20);
             this.textBox2.TabIndex = 9;
             // 
             // textBox3
             // 
             this.textBox3.Location = new System.Drawing.Point(92, 61);
             this.textBox3.Name = "textBox3";
-            this.textBox3.Size = new System.Drawing.Size(255, 20);
+            this.textBox3.Size = new System.Drawing.Size(50, 20);
             this.textBox3.TabIndex = 10;
             // 
             // generateButton
             // 
-            this.generateButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Bold);
+            this.generateButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Bold);
             this.generateButton.ForeColor = System.Drawing.Color.Green;
-            this.generateButton.Location = new System.Drawing.Point(205, 349);
+            this.generateButton.Location = new System.Drawing.Point(208, 349);
             this.generateButton.Name = "generateButton";
             this.generateButton.Size = new System.Drawing.Size(142, 51);
             this.generateButton.TabIndex = 11;
-            this.generateButton.Text = "Generate!";
+            this.generateButton.Text = "Save route!";
             this.generateButton.UseVisualStyleBackColor = true;
             this.generateButton.Click += new System.EventHandler(this.generateButton_Click);
             // 
@@ -156,7 +165,7 @@
             // 
             this.backButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Bold);
             this.backButton.ForeColor = System.Drawing.Color.Maroon;
-            this.backButton.Location = new System.Drawing.Point(12, 350);
+            this.backButton.Location = new System.Drawing.Point(12, 349);
             this.backButton.Name = "backButton";
             this.backButton.Size = new System.Drawing.Size(142, 51);
             this.backButton.TabIndex = 12;
@@ -164,20 +173,107 @@
             this.backButton.UseVisualStyleBackColor = true;
             this.backButton.Click += new System.EventHandler(this.backButton_Click);
             // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(92, 113);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(50, 20);
+            this.textBox1.TabIndex = 13;
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(92, 87);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(50, 20);
+            this.textBox4.TabIndex = 14;
+            // 
+            // minimumDurabilityLabel
+            // 
+            this.minimumDurabilityLabel.AutoSize = true;
+            this.minimumDurabilityLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.minimumDurabilityLabel.Location = new System.Drawing.Point(0, 90);
+            this.minimumDurabilityLabel.Name = "minimumDurabilityLabel";
+            this.minimumDurabilityLabel.Size = new System.Drawing.Size(90, 13);
+            this.minimumDurabilityLabel.TabIndex = 15;
+            this.minimumDurabilityLabel.Text = "Min. durability:";
+            // 
+            // minimumBagSlotsLabel
+            // 
+            this.minimumBagSlotsLabel.AutoSize = true;
+            this.minimumBagSlotsLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.minimumBagSlotsLabel.Location = new System.Drawing.Point(0, 116);
+            this.minimumBagSlotsLabel.Name = "minimumBagSlotsLabel";
+            this.minimumBagSlotsLabel.Size = new System.Drawing.Size(90, 13);
+            this.minimumBagSlotsLabel.TabIndex = 16;
+            this.minimumBagSlotsLabel.Text = "Min. bag slots:";
+            // 
+            // numberOfHotspotsLabel
+            // 
+            this.numberOfHotspotsLabel.AutoSize = true;
+            this.numberOfHotspotsLabel.Location = new System.Drawing.Point(9, 196);
+            this.numberOfHotspotsLabel.Name = "numberOfHotspotsLabel";
+            this.numberOfHotspotsLabel.Size = new System.Drawing.Size(104, 13);
+            this.numberOfHotspotsLabel.TabIndex = 18;
+            this.numberOfHotspotsLabel.Text = "Number of Hotspots:";
+            // 
+            // numberOfHotspots
+            // 
+            this.numberOfHotspots.AutoSize = true;
+            this.numberOfHotspots.Location = new System.Drawing.Point(119, 196);
+            this.numberOfHotspots.Name = "numberOfHotspots";
+            this.numberOfHotspots.Size = new System.Drawing.Size(13, 13);
+            this.numberOfHotspots.TabIndex = 19;
+            this.numberOfHotspots.Text = "0";
+            // 
+            // addHotspotButton
+            // 
+            this.addHotspotButton.Location = new System.Drawing.Point(241, 191);
+            this.addHotspotButton.Name = "addHotspotButton";
+            this.addHotspotButton.Size = new System.Drawing.Size(106, 32);
+            this.addHotspotButton.TabIndex = 20;
+            this.addHotspotButton.Text = "Add hotspot! / Add another!";
+            this.addHotspotButton.UseVisualStyleBackColor = true;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(3, 148);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(87, 13);
+            this.label1.TabIndex = 21;
+            this.label1.Text = "Enter hotspot:";
+            // 
+            // textBox5
+            // 
+            this.textBox5.Location = new System.Drawing.Point(6, 164);
+            this.textBox5.Name = "textBox5";
+            this.textBox5.Size = new System.Drawing.Size(344, 20);
+            this.textBox5.TabIndex = 22;
+            // 
             // GenerateRoute
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.SystemColors.ControlDarkDark;
             this.ClientSize = new System.Drawing.Size(359, 412);
+            this.Controls.Add(this.textBox5);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.addHotspotButton);
+            this.Controls.Add(this.numberOfHotspots);
+            this.Controls.Add(this.numberOfHotspotsLabel);
+            this.Controls.Add(this.minimumBagSlotsLabel);
+            this.Controls.Add(this.minimumDurabilityLabel);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.textBox1);
             this.Controls.Add(this.backButton);
             this.Controls.Add(this.generateButton);
             this.Controls.Add(this.textBox3);
             this.Controls.Add(this.textBox2);
             this.Controls.Add(this.nameOfRouteTB);
             this.Controls.Add(this.sellListBox);
-            this.Controls.Add(this.label2);
-            this.Controls.Add(this.label1);
+            this.Controls.Add(this.sellWhichItemsLabel);
+            this.Controls.Add(this.mailWhichItemsLabel);
             this.Controls.Add(this.mailListBox);
             this.Controls.Add(this.maximumLevelLabel);
             this.Controls.Add(this.minimumLevelLabel);
@@ -199,13 +295,22 @@
         private System.Windows.Forms.Label minimumLevelLabel;
         private System.Windows.Forms.Label maximumLevelLabel;
         private System.Windows.Forms.CheckedListBox mailListBox;
-        private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label mailWhichItemsLabel;
+        private System.Windows.Forms.Label sellWhichItemsLabel;
         private System.Windows.Forms.CheckedListBox sellListBox;
         private System.Windows.Forms.TextBox nameOfRouteTB;
         private System.Windows.Forms.TextBox textBox2;
         private System.Windows.Forms.TextBox textBox3;
         private System.Windows.Forms.Button generateButton;
         private System.Windows.Forms.Button backButton;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.Label minimumDurabilityLabel;
+        private System.Windows.Forms.Label minimumBagSlotsLabel;
+        private System.Windows.Forms.Label numberOfHotspotsLabel;
+        private System.Windows.Forms.Label numberOfHotspots;
+        private System.Windows.Forms.Button addHotspotButton;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox5;
     }
 }
